5b350495e7b8318dd3ba2340feec13080b248faa
Zhexin Liu <zhexinliu2018@gmail.com>
2022-01-25 17:10:50 +0100

    made some changes

--- code/Baseline.py
+++ code/Baseline.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
+# Usage: python3 -t int -o out_path
 # In[146]:
 
 
 ## prepare data
 
 
-# In[11]:
+# In[16]:
 
 
-get_ipython().run_line_magic('matplotlib', 'inline')
+
 import pandas as pd; import numpy as np 
 import os
 from sklearn.utils import resample
@@ -22,22 +23,22 @@ import math
 import matplotlib.pyplot as plt
 import warnings
 from functools import reduce
-import importlib
-from tools import write_file
+import argparse
 import multiprocessing
 import tools
 import importlib
 importlib.reload(tools)
 
 
-# In[2]:
+# In[25]:
 
 
 #load four mutation files.
-path = '/Users/jerryliu/Documents/Vu_uva/internship/CCLE/mutation_files/'
+path = '/scratch/CCLE/mutation_files/'
 file_list = ['damaging','hotspot','nonconserving','otherconserving']
-drug_path = '/Users/jerryliu/Documents/Vu_uva/internship/CCLE/primary-screen-replicate-collapsed-logfold-change.csv'
-out_path = '/Users/jerryliu/jerry_jupyter/internship/output/'
+drug_path = '/scratch/CCLE/primary-screen-replicate-collapsed-logfold-change.csv'
+#out_path = '/Users/jerryliu/jerry_jupyter/internship/output/'
+Con_file='/scratch/CCLE/mutation_files/Census_allSat.csv'
 
 
 # In[3]:
@@ -87,7 +88,7 @@ def filter_genes(file, thresh = 0.1):
     
 
 
-# In[15]:
+# In[66]:
 
 
 
@@ -133,7 +134,7 @@ def out_loop (x, y_total, cl_id, out_cv_fold= 5):
         # each pair of alpha and l1_ratio, do cross training to sellect best pair. 
 
         l1_ratio_list = np.linspace(start = 0.2, stop = 1.0, num = 10) #10 values
-        alpha_list =  np.array([math.exp(i) for i in np.arange(-16,5,2)]) # 250 values
+        alpha_list =  np.array([math.exp(i) for i in np.arange(-150,15,20)] ) # 250 values
         #alpha_list =  np.array([math.exp(i) for i in np.arange(-8,5,0.8)]) #  values
         para_matrix = {(l1_ratio, alpha):0 for l1_ratio in l1_ratio_list for alpha in alpha_list}
 
@@ -141,11 +142,11 @@ def out_loop (x, y_total, cl_id, out_cv_fold= 5):
             #do ten fold cv to sellect best parameter pairs. 
             ave_per = EN_cv_in(x_train= x_train, y_train = y_train, fold = 10, alpha = alpha, l1_ratio= l1_ratio)
             para_matrix[(l1_ratio, alpha)] = ave_per
-        print(para_matrix)
+        #print(para_matrix)
         
         #return the best alpha-l1-ratio pair.
         op_l1_ratio, op_alpha = pd.Series(para_matrix).idxmax() 
-        print('the best validation performance is: ', para_matrix[(op_l1_ratio, op_alpha)])
+        #print('the best validation performance is: ', para_matrix[(op_l1_ratio, op_alpha)])
         
         #predict on the 5th-fold set (testing set)
         with warnings.catch_warnings():
@@ -160,50 +161,35 @@ def out_loop (x, y_total, cl_id, out_cv_fold= 5):
         for index, value in zip(test_label, y_pre): pre_y_array[index].append(value)
         per_test = pd.Series(y_pre).corr(pd.Series(y_test),method = 'spearman') 
         per_test_list.append(per_test)
-        print('performance on the test set is:', per_test)
-        plt.scatter(y_test, y_pre)
-        plt.xlabel('test set')
-        plt.ylabel('prediction')
-        plt.show()
-        #break
+        # print('performance on the test set is:', per_test)
+        # plt.scatter(y_test, y_pre)
+        # plt.xlabel('test set')
+        # plt.ylabel('prediction')
+        # plt.show()
+        # #break
     #add the prediction to the list.
     pre_y_array = pd.Series(pre_y_array).reindex(cl_id)
     
     return(pre_y_array, per_test_list)
 
-def drug_model(comp_index, m_file,  drug_data, out_path):
+
+def drug_model(m_file, target, target_index, out_path):
     """
     input: m_file mutation matrix with unpreprocessed binery values.
     output: mean prediction values of 10 iterations in pd.Sereis type.
     """
-    #eliminate cell lines with nan dryg sensitivity data.
-    #comp_index = 0
-#     print(comp_index)
-#     print(m_file.shape)
-    
-    valid_cl_index = drug_data.loc[m_file.index,:].iloc[:,comp_index].dropna().index
-    Y = drug_data.iloc[:, comp_index][valid_cl_index]
-    drug_name = drug_data.columns[comp_index]
-    print('drug_name is: ',drug_name)
-    print('number of missing values in Y: ', Y.isnull().sum())
-    m_file_notna = m_file.loc[valid_cl_index,:]
-    
+    #eliminate cell lines with nan drug sensitivity data.
+    drug_name = target.name
+    Y = target.dropna()
+    m_file_notna = m_file.loc[Y.index,:]
+
     #normalize the each col to have ~0 mean and ~1 sd. 
     norm_m_file = (m_file_notna - m_file_notna.mean(axis = 0)) / m_file_notna.std(axis = 0)
-    print(norm_m_file.sum())
+    #print(norm_m_file.sum())
     
-    print('### normalize with mean=0, std = 1.')
+    #print('### normalize with mean=0, std = 1.')
     norm_m_file.dropna(axis = 1, inplace = True)
-    
-#     #Use features that have pearson correlation with Y over 0.1.
-#     cor_number = norm_m_file.corrwith(Y, axis = 0, method = 'pearson')
-#     #number of genes that have correlation over 0.1
-#     #sum(np.abs(cor_number) >= 0.1)
-#     norm_m_file = norm_m_file.loc[:, np.abs(cor_number) >= 0.1]
-#     #print('number of features selected {}'.format(sum(np.abs(cor_number) >= 0.1)))
-    
-    
-    print('number of features selected {}'.format(norm_m_file.shape[1]))
+    #print('number of features selected {}'.format(norm_m_file.shape[1]))
     X, Y_total = norm_m_file, Y
     
     ### EN model
@@ -214,11 +200,11 @@ def drug_model(comp_index, m_file,  drug_data, out_path):
     Y_pre_array = pd.Series(dict(list(zip(cl_id, [[] for i in range(cl_id.shape[0])]))),
                             index = cl_id, name = drug_name)
     cv_result_list = []
-    for i in tqdm(range(10)):
-        print('iteration {} begains'.format(i))
+    for i in range(10):
+        #print('iteration {} begains'.format(i))
         x, y_total = resample(X, Y_total, replace = False, n_samples = int(X.shape[0]*0.8), random_state = i)
-        print(X.isna().sum().sum())
-        print(y_total.isna().sum().sum())
+        #print(X.isna().sum().sum())
+        #print(y_total.isna().sum().sum())
 
         #outter loop with 5 fold cv.
         prediction, cv_list = out_loop(x = x, y_total= y_total, cl_id = cl_id, out_cv_fold=5 )
@@ -229,70 +215,97 @@ def drug_model(comp_index, m_file,  drug_data, out_path):
 
     
     ##calculate the mean of 10 iterations. 
-    Y_pre_array = Y_pre_array.map(np.mean)
-    tools.write_file(Y_pre_array, 'comp_{}.csv'.format(comp_index), out_path)
+    #Y_pre_array = Y_pre_array.map(np.mean)
+    tools.write_file(Y_pre_array, out_path, 'comp{}.csv'.format(target_index+1) )
     # list of cross validation results. 
     return(cv_result_list)
 
 
-# In[7]:
+# In[47]:
 
 
-#def main():
-#read data
-drug_data = read_drug(drug_path = drug_path)
-m_f1, m_f2, m_f3,_  = file_list
-m_f1 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f1 + '.csv', index_col = 0)
-m_f2 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f2 + '.csv', index_col = 0)
-m_f3 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f3 + '.csv', index_col = 0)
-print('#### reading files completed')
+def main():
 
-#merge mutation files
-m_file = merge_mt(merge_mt(m_f1, m_f2, drug_id=drug_data.index), m_f3, 
-                  drug_id= drug_data.index,gene_filter=False)
-#m_file = m_f1.reindex(set.intersection(set(m_f1.index), set(drug_data.index)))
-print('#### merge mutation files completed')
-print(m_file.shape)
-#filter most mutated genes. 
-w_m_file = merge_mt(merge_mt(m_f1, m_f2, drug_id=drug_data.index, gene_filter=True), m_f3, 
-                  drug_id= drug_data.index,gene_filter = True)
+    #read data
+    drug_data = read_drug(drug_path = drug_path)
+    m_f1, m_f2, m_f3,_  = file_list
+    m_f1 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f1 + '.csv', index_col = 0)
+    m_f2 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f2 + '.csv', index_col = 0)
+    m_f3 = pd.read_csv(path + 'CCLE_mutations_bool_' + m_f3 + '.csv', index_col = 0)
+#print('#### reading files completed')
 
-#w_m_file = m_f1
-#m_file = m_f1
-gene_bool = filter_genes(w_m_file, thresh=0.08)
-#print(gene_bool)
-m_file = m_file.loc[:,gene_bool]
-print(m_file.shape)
 
-# get rid of drugs with too many missing values.
-thresh  = int(m_file.shape[0]*(1-0.1)) ## at least ## number of Non-na value in each drug
-drug_id_list = drug_data.loc[m_file.index,:].dropna(axis = 1, thresh = thresh).columns
-#build model for each drug.
-pre_matrix = pd.Series(index = m_file.index)
 
-''' multiprocessing the command '''
-n_jobs = 100
-jobs_list = tools.split_task(len(drug_id_list), n_jobs)
-drug_mapping_list = list(zip(drug_id_list, [np.where(drug_data.columns == drug_id)
-for index_array in jobs_list:
-    work_list = []
-    for job in index_array:
-        drug_name, drug_index = drug_mapping_list[job][0], int(drug_mapping_list[job][1][0])
-        p = multiprocessing.Process(target = drug_model, args = (drug_index, m_file, drug_data))
-        work_list.append(p)
-    #start the programs.
-    for job in work_list: job.start()
-    #end the programs.
-    for job in work_list: job.join()
+# In[48]:
 
-# for index, (drug_name, drug_index) in enumerate(list(zip(drug_id_list, [np.where(drug_data.columns == drug_id) for drug_id in drug_id_list]))):
-#     if index == 3:
-#         result = drug_model(comp_index = int(drug_index[0]), m_file = m_file, drug_data=drug_data,
-#                        out_path= out_path)
-#     #pre_matrix = pd.concat([pre_matrix, result], axis = 1)
-    
 
-    
+    gene_bankmap = {gene.split(' ')[0]:gene.split(' ')[1]
+     for gene in tools.get_cons(m_f1.columns, m_f2.columns, m_f3.columns, method='union')}
+
+    Con_genes = pd.read_csv(Con_file).loc[:,'Gene Symbol']
+    gene_lists =  [ m_file.columns.map(lambda x:x.split(' ')[0])
+        for m_file in [m_f1, m_f2, m_f3] ]
+    final_genes = tools.get_cons(tools.get_cons(*gene_lists, method='union'),
+                   Con_genes, method = 'inter')
+    final_genes = [f"{gene} {gene_bankmap[gene]}" for gene in final_genes ]
+
+
+# In[49]:
+
+
+    #merge mutation files
+    m_file = merge_mt(merge_mt(m_f1, m_f2, drug_id=drug_data.index), m_f3,
+                      drug_id= drug_data.index,gene_filter=False)
+    #m_file = m_f1.reindex(set.intersection(set(m_f1.index), set(drug_data.index)))
+
+
+
+# In[50]:
+
+
+    #filter most mutated genes.
+    #w_m_file = merge_mt(merge_mt(m_f1, m_f2, drug_id=drug_data.index, gene_filter=True), m_f3,
+    #                  drug_id= drug_data.index,gene_filter = True)
+    #gene_bool = filter_genes(w_m_file, thresh=0.08)
+
+    #take genes intersecting the COSMIC data
+
+
+    m_file = m_file.loc[:,final_genes]
+    print(m_file.shape)
+
+    # get rid of drugs with too many missing values.
+    thresh  = int(m_file.shape[0]*(1-0.1)) ## at least ## number of Non-na value in each drug
+    drug_df = drug_data.loc[m_file.index,:].dropna(axis = 1, thresh = thresh)
+    #print(f'before filtering drugs: the number of drugs is {drug_data.shape[1]}')
+    #print(f'after filtering, the number of drugs is {len(drug_id_list)}')
+
+
+# In[ ]:
+    comp_index = dg_index
+    target = drug_df.iloc[:, comp_index]
+    pre = drug_model(m_file, target, comp_index, out_path=out_path)
+
+
+
+
+
+    #n_jobs = 100
+    # jobs_list = tools.split_task(len(drug_id_list), n_jobs)
+    # drug_mapping_list = (list(zip(drug_id_list, 
+    #                           [np.where(drug_data.columns == drug_id) for drug_id in drug_id_list])))
+
+
+    # for index_array in jobs_list:
+    #     work_list = []
+    #     for job in index_array:
+    #         drug_name, drug_index = drug_mapping_list[job][0], int(drug_mapping_list[job][1][0])
+    #         p = multiprocessing.Process(target = drug_model, args = (drug_index, m_file, drug_data))
+    #         work_list.append(p)
+    #     #start the programs.
+    #     for job in work_list: job.start()
+    #     #end the programs.
+    #     for job in work_list: job.join()
     
 
 
@@ -300,5 +313,15 @@ for index_array in jobs_list:
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target_index", type=int,
+                        help= "target index (1-based)")
+    parser.add_argument("-o","--output", type = str,
+                        help= "output file's dir ")
+    args = parser.parse_args()
+    
+    dg_index = (args.target_index -1 ) #convert to 0-based
+    out_path = args.output
+    
     main()
 
